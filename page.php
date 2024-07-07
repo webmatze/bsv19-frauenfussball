8db3e7669117205deb1ba036e691543aef776cd2
Mathias Karstädt <mathias.karstaedt@gmail.com>
2024-07-07 15:31:32 +0200

    fixes page.php layout

--- page.php
+++ page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all pages.
  *
@@ -12,33 +13,31 @@
  * @package BSV19
  */
 
-get_header(); 
+get_header();
 
 $sidebar_layout = the_minimal_sidebar_layout();
-    
+
 ?>
 
-  <div>
-    <div class="w-container">
-      <h2 class="heading-2">Spaßkick</h2>
+<div class="kontakt">
+	<div class="w-container">
+		<h2 class="heading-2"><?php the_title(); ?></h2>
+		<?php
+		while (have_posts()) : the_post();
+			the_content();
 
-			<?php
-			while ( have_posts() ) : the_post();
+			// If comments are open or we have at least one comment, load up the comment template.
+			if (comments_open() || get_comments_number()) :
+				comments_template();
+			endif;
 
-				get_template_part( 'template-parts/content', 'page' );
-                
-                // If comments are open or we have at least one comment, load up the comment template.
-    			if ( ( comments_open() || get_comments_number() ) ) :
-    				comments_template();
-    			endif;
-				
-			endwhile; // End of the loop.
-			?>
-    </div>
-  </div>
+		endwhile; // End of the loop.
+		?>
+	</div>
+</div>
 
 
 <?php
-if( $sidebar_layout == 'right-sidebar' )
-  get_sidebar();
+if ($sidebar_layout == 'right-sidebar')
+	get_sidebar();
 get_footer();
