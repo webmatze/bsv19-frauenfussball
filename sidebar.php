82970e372c5d28d746faf2b9578598ca093cc5fe
Mathias Karstädt <mathias.karstaedt@gmail.com>
2024-07-07 19:46:15 +0200

    make contents of sidebar configurable

--- sidebar.php
+++ sidebar.php
@@ -10,15 +10,7 @@
 if (is_active_sidebar('right-sidebar')) : ?>
   <div class="section grey">
     <div class="w-container">
-      <div class="secondary-row w-row">
-        <div class="w-col w-col-9 w-col-small-6">
-          <h4>Wir suchen dich! Jetzt für ein kostenloses Probetraining anmelden!</h4>
-          <p>Wir sind immer auf der Suche nach interessierten neuen Spielerinnen egal welchen Alters. Ob du Anfängerin bist, oder bereits seit längerem Fußball spielst, ob du nur zum Spaß kicken oder bei unseren Teams mitspielen möchtest – wir freuen uns auf deinen Besuch bei uns in Bahrenfeld.<br><br>Melde dich einfach unverbindlich zu einem kostenlosen Probetraining an und lerne so unsere Spielerinnen kennen.</p>
-        </div>
-        <div class="button-column w-col w-col-3 w-col-small-6">
-          <a href="probetraining.html" class="button">Probetraining anmelden</a>
-        </div>
-      </div>
+      <?php dynamic_sidebar('right-sidebar'); ?>
     </div>
   </div>
 <?php endif; ?>
